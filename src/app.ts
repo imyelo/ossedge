e71946c464b425f4ede3ca782109307cd2a31a87
Yelo <zhihuzeye@gmail.com>
2024-09-12 04:46:37 +0800

    feat: add cache

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import OSS from 'ali-oss'
+import { caching } from 'cache-manager'
+import { DiskStore } from 'cache-manager-fs-hash'
 import { appendHeader, createApp, defineEventHandler, sendNoContent, setResponseStatus } from 'h3'
 import { Readable } from 'stream'
-import { APP_OSS_ACCESS_KEY_ID, APP_OSS_ACCESS_KEY_SECRET, APP_OSS_BUCKET_NAME, APP_OSS_REGION } from './env.js'
+import {
+  APP_CACHE_DIR,
+  APP_CACHE_MAX_ITEMS,
+  APP_CACHE_TTL,
+  APP_OSS_ACCESS_KEY_ID,
+  APP_OSS_ACCESS_KEY_SECRET,
+  APP_OSS_BUCKET_NAME,
+  APP_OSS_REGION,
+} from './env.js'
 
 const oss = new OSS({
   region: APP_OSS_REGION,
@@ -11,28 +21,53 @@ const oss = new OSS({
 })
 
 export const app = createApp()
-
-app.use(
-  '/',
-  defineEventHandler(async event => {
-    try {
-      const path = event.path === '/' ? 'index.html' : event.path.slice(1)
-
-      if (path === 'favicon.ico') {
-        return sendNoContent(event)
-      }
-
-      const result = await oss.get(path)
-
-      appendHeader(event, 'Content-Type', result.res.headers['content-type'])
-      appendHeader(event, 'Content-Length', result.res.headers['content-length'])
-      appendHeader(event, 'Cache-Control', 'public, max-age=31536000, immutable, no-transform')
-      appendHeader(event, 'Expires', new Date(Date.now() + 31536000000).toUTCString())
-
-      return Readable.from(result.content)
-    } catch (error) {
-      setResponseStatus(event, 404)
-      return 'File not found'
-    }
+;(async () => {
+  const store = new DiskStore({
+    path: APP_CACHE_DIR,
+    max: APP_CACHE_MAX_ITEMS,
+    ttl: APP_CACHE_TTL,
+    subdirs: true,
+    zip: false,
   })
-)
+  const fsCache = await caching(store)
+
+  app.use(
+    '/',
+    defineEventHandler(async event => {
+      try {
+        const filePath = event.path === '/' ? 'index.html' : event.path.slice(1)
+
+        if (filePath === 'favicon.ico') {
+          return sendNoContent(event)
+        }
+
+        const cached = await fsCache.get<{ content: Buffer; headers: Record<string, string> }>(filePath)
+        if (cached) {
+          const { content, headers } = cached
+          Object.entries(headers).forEach(([key, value]) => {
+            appendHeader(event, key, value)
+          })
+          return Readable.from(content)
+        }
+
+        const result = await oss.get(filePath)
+        const headers = {
+          'Content-Type': result.res.headers['content-type'],
+          'Content-Length': result.res.headers['content-length'],
+          'Cache-Control': 'public, max-age=31536000, immutable, no-transform',
+          Expires: new Date(Date.now() + 31536000000).toUTCString(),
+        }
+
+        await fsCache.set(filePath, { content: result.content, headers })
+
+        Object.entries(headers).forEach(([key, value]) => {
+          appendHeader(event, key, value)
+        })
+        return Readable.from(result.content)
+      } catch (error) {
+        setResponseStatus(event, 404)
+        return 'File not found'
+      }
+    })
+  )
+})()
--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,10 @@
 import dotenvFlow from 'dotenv-flow'
+import path from 'path'
 
 dotenvFlow.config()
 
 export const APP_PORT = +process.env.APP_PORT
 export const { APP_OSS_REGION, APP_OSS_ACCESS_KEY_ID, APP_OSS_ACCESS_KEY_SECRET, APP_OSS_BUCKET_NAME } = process.env
+export const APP_CACHE_DIR = path.resolve(process.cwd(), process.env.APP_CACHE_DIR)
+export const APP_CACHE_TTL = +process.env.APP_CACHE_TTL
+export const APP_CACHE_MAX_ITEMS = +process.env.APP_CACHE_MAX_ITEMS
