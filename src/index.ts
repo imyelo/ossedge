7e7cfa90bc02560e514c91cf21194b8af9f95a5d
Yelo <zhihuzeye@gmail.com>
2024-09-11 21:58:55 +0800

    feat: mirror oss files

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import OSS from 'ali-oss'
+import dotenvFlow from 'dotenv-flow'
+import { appendHeader, createApp, defineEventHandler, sendNoContent, setResponseStatus } from 'h3'
+import { Readable } from 'stream'
+
+dotenvFlow.config()
+
+const oss = new OSS({
+  region: process.env.APP_OSS_REGION,
+  accessKeyId: process.env.APP_OSS_ACCESS_KEY_ID,
+  accessKeySecret: process.env.APP_OSS_ACCESS_KEY_SECRET,
+  bucket: process.env.APP_OSS_BUCKET_NAME,
+})
+
+export const app = createApp()
+
+app.use(
+  '/',
+  defineEventHandler(async event => {
+    try {
+      const path = event.path === '/' ? 'index.html' : event.path.slice(1)
+
+      if (path === 'favicon.ico') {
+        return sendNoContent(event)
+      }
+
+      const result = await oss.get(path)
+
+      appendHeader(event, 'Content-Type', result.res.headers['content-type'])
+      appendHeader(event, 'Content-Length', result.res.headers['content-length'])
+      appendHeader(event, 'Cache-Control', 'public, max-age=31536000, immutable, no-transform')
+      appendHeader(event, 'Expires', new Date(Date.now() + 31536000000).toUTCString())
+
+      return Readable.from(result.content)
+    } catch (error) {
+      setResponseStatus(event, 404)
+      return 'File not found'
+    }
+  })
+)
