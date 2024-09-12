607947bc2ab967c9200e29965e1e56d8629dd4ae
Yelo <zhihuzeye@gmail.com>
2024-09-12 16:28:21 +0800

    test: add tests

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,24 @@
 import test from 'ava'
+import { toNodeListener } from 'h3'
+import supertest from 'supertest'
+import { app } from '../src/app.js'
 
-test('defaults', async t => {
-  t.pass()
+const listener = toNodeListener(app)
+
+test('reply 204 for favicon', async t => {
+  const request = supertest(listener)
+  const response = await request.get('/favicon.ico')
+  t.is(response.status, 204)
+})
+
+test('reply 200 for /uploads/test.png', async t => {
+  const request = supertest(listener)
+  const response = await request.get('/uploads/test.png')
+  t.is(response.status, 200)
+})
+
+test('reply 404 for /not-found', async t => {
+  const request = supertest(listener)
+  const response = await request.get('/not-found')
+  t.is(response.status, 404)
 })
